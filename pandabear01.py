62ebbbdbac38f1fdf1d67bbfe20b41cd90eb964a
Jeffrey Lee <dr3adnought@protonmail.com>
2022-04-08 13:14:30 -0400

    first commit pandabear01 and files

---
+++ pandabear01.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# import subprocess
+# import sys
+import pip
+import importlib
+import pandas as pd
+
+
+def import_with_auto_install(package):
+    try:
+        return importlib.import_module(package)
+    except ImportError:
+        pip.main(['install', package])
+    return importlib.import_module(package)
+
+
+if __name__ == '__main__':
+    pandas = import_with_auto_install('pandas')
+    print(pandas)
+    xlrd = import_with_auto_install('xlrd')
+    print(xlrd)
+    openpyxl = import_with_auto_install('openpyxl')
+    print(openpyxl)
+    matplotlib = import_with_auto_install('matplotlib')
+    print(matplotlib)
+
+
+def main():
+    # define the name of our xls file
+    excel_file = 'movies.xls'
+
+    # create a DataFrame (DF) object. EASY!
+    # because we did not specify a sheet
+    # only the first sheet was read into the DF
+    movies = pd.read_excel(excel_file)
+
+    # show the first five rows of our DF
+    # DF has 5 rows and 25 columns (indexed by integer)
+    print(movies.head())
+
+    # Choose the first column "Title" as
+    # index (index=0)
+    movies_sheet1 = pd.read_excel(excel_file, sheet_name=0, index_col=0)
+
+    # DF has 5 rows and 24 columns (indexed by title)
+    # print the top 10 movies in the dataframe
+    print(movies_sheet1.head())
+
+    # export 5 movies from the top dataframe to excel
+    movies_sheet1.head(5).to_excel("5movies.xlsx")
+
+    # export 5 movies from the top of the dataframe to json
+    movies_sheet1.head(5).to_json("5movies.json")
+
+    # export 5 movies from the top of the dataframe to csv
+    movies_sheet1.head(5).to_csv("5movies.csv")
+
+
+if __name__ == "__main__":
+    main()
